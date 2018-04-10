d0d94fe64d3d6bec545066b3eefe75408ba0945d
Federico <federico.sepulveda@alumnos.unc.edu.ar>
2018-04-10 01:45:08 -0300

    arreglar promt

--- tp1/tp1_cliente_baash/tp1_cliente_baash.c
+++ tp1/tp1_cliente_baash/tp1_cliente_baash.c
@@ -12,18 +12,45 @@
 #include <netdb.h> 
 #define TAM 2048
 
+/*!
+  Función principal del cliente.
+*/
+int parsear_entrada(char *entrada,char *entrada_parseada[],char *caracteres);
+int autenticar(int sockfd,char *usuario);
+
 int main( int argc, char *argv[] ) {
 	int sockfd, puerto, n;
 	struct sockaddr_in serv_addr;
 	struct hostent *server;
 	int terminar = 0;
-
+	int longitud=0,autenticacion=0;
+	char* entrada_parseada[TAM];
+	char entrada[TAM];
 	char buffer[TAM];
 	if ( argc < 3 ) {
 		fprintf( stderr, "Uso %s host puerto\n", argv[0]);
 		exit( 0 );
 	}
-
+	///////////////////******************CONEXION***************************
+	// printf( "\nCOP: " );
+	// memset( entrada, '\0', TAM );// se coloca un \0 en entrada en la pocision al final de entrada
+	// ///\par Se le pide al usuario que ingrese connect seguido por usuario@numero_ip:puerto 
+	// fgets( entrada, TAM-1, stdin );//se lee el archivo stdin (lee lo que se ingreso) y se copia en entrada 
+	
+	// // strcpy(entrada,"connect sepulveda@192.168.0.4:6020");//SOLO PARA PRUEBA DESCOMENTAR ARRIBA
+	// entrada[strlen(entrada)-1] = '\0';// se elimina el \n que es introducido por el usuario cuando preciona "enter"
+	// longitud=parsear_entrada(entrada,entrada_parseada," @:");
+	
+	// while( strcmp( "connect", entrada_parseada[0]) ){
+	// 		printf( "\nComando desconocido debe ingresar connect seguido usuario@numero_ip:puerto del AWS" );
+	// 		printf( "\nCOP: " );
+	// 		memset( entrada, '\0', TAM );// se coloca un \0 en entrada en la pocision al final de entrada
+	// 		fgets( entrada, TAM-1, stdin );//se lee el archivo stdin (lee lo que se ingreso) y se copia en entrada 
+	// 		entrada[strlen(entrada)-1] = '\0';// se elimina el \n que es introducido por el usuario cuando preciona "enter"
+	// 		longitud=parsear_entrada(entrada,entrada_parseada," @:");
+			
+	// }
+	///////////////********************************************************************************************
 	puerto = atoi( argv[2] );
 	sockfd = socket( AF_INET, SOCK_STREAM, 0 );
 	if ( sockfd < 0 ) {
@@ -46,7 +73,19 @@ int main( int argc, char *argv[] ) {
 	}
 
 	while(1) {
-		printf( "Ingrese el mensaje a transmitir: " );
+		// printf( "Ingrese el mensaje a transmitir: " );
+
+		// ///\par Se llama a la funcion autenticacion.
+		// while(autenticacion==0){
+		// 	autenticacion=autenticar(sockfd,entrada_parseada[1]);
+		// }
+		// ///\par Si fallo la acutenticion se cierra la aplicacion cliente
+		// if (autenticacion==-1){
+		// 	printf( "Supero el numero de intentos pemitidos\n" );
+		// 	close(sockfd);
+		// 	exit(0);
+		// }
+
 		memset( buffer, '\0', TAM );
 		fgets( buffer, TAM-1, stdin );
 
@@ -69,7 +108,8 @@ int main( int argc, char *argv[] ) {
 			exit( 1 );
 		}
 
-		printf( "Respuesta: %s\n", buffer );
+
+		printf( "%s\n", buffer );
 		if( terminar ) {
 			printf( "Finalizando ejecución\n" );
 			exit(0);
@@ -81,8 +121,78 @@ int main( int argc, char *argv[] ) {
 			perror( "lectura de socket" );
 			exit( 1 );
 		}
-		printf( "Respuesta: %s\n", buffer );
+		printf( "%s", buffer );/// se imprime promt()
 
 	}
 	return 0;
 } 
+
+
+/*!
+  \param entrada es la cadena que se quiere parsear.
+  \param entrada_parseada[] es donde se va almacenando la cadena ya parseada.
+  \param caracteres son los caracteres que se quieren quitar de la cadena
+   \return Devuelve la longitud de entrada_parseada.
+*/
+///\par Función parsear_entrada.
+/// Esta funcion quita la los simbolos (que se le pasan caracteres). Es util para transformar una cadena en una lista de cadenas
+int parsear_entrada(char *entrada,char *entrada_parseada[],char *caracteres){ // se ingresa cada palabra a una posicion del vector entrada_parseada
+	int i=0;
+	char *token; 
+	token = strtok( entrada, caracteres); //se quitan los "caracteres"
+	entrada_parseada[i]=token;// 
+	while(token!=NULL){	
+		i++;
+		token = strtok( NULL, caracteres);//se siguen quitando los "caracteres"
+		entrada_parseada[i]=token;
+	}
+	return i;
+}
+
+/*!
+  \param sockfd es el file descriptor utilizado para realizar la comunicaciones TCP.
+  \param usuario es el nombre de usuario.
+   \return Devuelve 1 si la autenticacion fue exitosa, -1 si se supero en numero de intentos por parte del servidor y 0 si la clave o usuario son incorrectos.
+*/
+///\par Función autenticar.
+/// Esta funcion se encarga de la autencicacion con el servidor. Se le envia a destino el usuario con la clave ingresada
+///con formato "usuario,clave", esta cadena se compara con los datos almacenados en la tabla. 
+int autenticar(int sockfd,char *usuario){
+	char entrada[TAM],buffer_receptor[TAM],concatenar[TAM];
+	int n;
+	printf( "Usuario: %s\nIngrese la clave\nCOP: ", usuario);
+	strcpy(concatenar,usuario);
+	/// Se le pide al usuario que ingrese la clave correspondiete
+	fgets( entrada, TAM, stdin );//se lee el archivo stdin (lee lo que se ingreso) y se copia en entrada 
+	strcat(concatenar,",");
+	/// Se concatena la el nombre de usuario con la clave y se le da el formato para ser leido por el destino.
+	strcat(concatenar,entrada);
+	concatenar[strlen(concatenar)-1] = '\0';//quita el \n que esta al final de linea
+	/// Se envia la cadena
+	n = write( sockfd, concatenar, strlen(concatenar) );//envia usuario/clave
+	if ( n < 0 ) {
+		perror( "escritura de socket" );
+		exit( 1 );
+	}
+	/// Se espera respuesta por parte del servidor
+	n = read( sockfd, buffer_receptor, TAM );//leer socket (respueta OBLIGATORIA por parte del servidor)
+	if ( n < 0 ) {
+		perror( "lectura de socket" );
+		exit( 1 );
+	}
+	/// - Si es "ok" significa que la clave ingresada fue correcta.
+	if( !strcmp( "ok", buffer_receptor) ){
+		return 1; 
+	}
+	/// - Si es "superoIntentos" significa que finalizo la autenticacion por superar la cantidad de intentos
+	else if( !strcmp( "superoIntentos", buffer_receptor) ) {
+		printf("\nSupero la cantidad de intentos\n");
+		return -1; 
+	}
+	/// - Si es "no" es porque la clave o usuarios no son correctos
+	else{
+		printf("\nNombre de usuario y/o contraseña incorrecto\n");
+		return 0;	
+	}
+
+}
--- tp1/tp1_servidor_baash/tp1_servidor_baash.c
+++ tp1/tp1_servidor_baash/tp1_servidor_baash.c
@@ -11,12 +11,13 @@
 #include <netinet/in.h>
 #include "baash.h"
 #define TAM 256
+#define cantida_intentos 4
 
 int main( int argc, char *argv[] ) {
 	int sockfd, newsockfd, puerto, clilen, pid;
 	char buffer[TAM];
 	struct sockaddr_in serv_addr, cli_addr;
-	int n;
+	int n,autenticacion=0;
 
 	if ( argc < 2 ) {
         	fprintf( stderr, "Uso: %s <puerto>\n", argv[0] );
@@ -61,11 +62,16 @@ int main( int argc, char *argv[] ) {
 
 		if ( pid == 0 ) {  // Proceso hijo
 			close( sockfd );
-			
+			// ///\par Se realiza la autenticacion. Espera la contraseña por parte del usuario. Solo tiene cuatro intentos.
+			// autenticacion=autenticar(newsockfd);
 
 			while ( 1 ) {
 				
-  				
+  		// 		if(autenticacion==0){
+				// 	printf( "PROCESO %d. Se supero los intentos de autenticacion, termino la ejecución.\n\n", getpid() );
+				// 	close( sockfd );close( newsockfd );
+				// 	exit(0);
+				// }
 				// memset( buffer, '\0', TAM );
 
 				// n = read( newsockfd, buffer, TAM-1 );
@@ -74,8 +80,8 @@ int main( int argc, char *argv[] ) {
 				// 	exit(1);
 				// }
 
-				printf( "Server->PROCESO %d. ", getpid() );
-				printf( "Recibí: %s", buffer );
+				//printf( "Server->PROCESO %d. ", getpid() );
+				//printf( "Recibí: %s", buffer );
 				
 				// n = write( newsockfd, "Obtuve su mensaje", 18 );
 				// if ( n < 0 ) {
@@ -98,3 +104,84 @@ int main( int argc, char *argv[] ) {
 	}
 	return 0; 
 } 
+
+
+
+/*!
+  \param newsockfd es el file descriptor utilizado para realizar la comunicaciones TCP.
+   \return Devuelve 1 si la clave fue correcta 0 si no lo fue.
+*/
+///\par Función autenticacion.
+/// Se utiliza un archivo usuarios.csv inicialmente pre cargado (si no la aplicacion no va a poder funcionar) con nomebres de usuarios
+///y las correspondientes claves. Se espera en mensaje por parte del cliente para realizar las comparaciones con los datos del archivo.
+///En total el cliente tiene 4 intentos de ingresar la clave de forma incorrecta en caso que no lo logre en niguno de esos intentos se 
+///avisa al cliente y se retorna 0. En caso de una conicidencia exitosa se le avisa al usuario y retorna 1.
+
+int autenticar(int newsockfd){
+	int n, logitud;
+	/**
+	* \par Variable intentos:
+	* Numero de oportunidades que tiene el usuario para intentar ingresar mal la clave.
+	*/
+	int intentos=cantida_intentos;
+	char buffer[TAM], usuario[TAM],buffer_archivo[TAM];
+	char* buffer_parseado[TAM];
+
+	FILE *usuarios;
+	// usuarios = fopen("usuarios.csv","r");
+	
+	while(intentos>0){
+		/// Se lee el archivo usuarios.csv el cual es una tabla usuario/clave
+		usuarios = fopen("usuarios.csv","r");
+		memset( buffer_archivo, '\0', TAM );
+		memset( buffer, '\0', TAM );
+		/// Espera usuario/clave por parte del cliente		
+		n = read( newsockfd, buffer, TAM ); 
+		if ( n < 0 ) {
+			perror( "lectura de socket" );
+			exit(1);
+		}
+		/// Comienza a comparar el dato recibido con todos los datos en la tabla usuarios.csv
+		fgets(buffer_archivo, TAM, usuarios);
+		while (!feof (usuarios)){
+			
+			buffer_archivo[strlen(buffer_archivo)-1] = '\0';
+			// buffer[strlen(buffer)-1] = '\0';
+			/// Si los datos coinciden se avisa a destino que la secion pudo iniciar con exito y se retorna 1.
+			if( !strcmp( buffer_archivo,buffer) ){
+				//se encotro el usuario con la clave
+				n = write( newsockfd, "ok", TAM ); // avisa al destino que se encontro usuario/clave
+				if ( n < 0 ) { 
+					perror( "lectura de socket" );
+					exit( 1 );
+				}
+				fclose(usuarios);
+				memset( buffer, '\0', TAM );
+				return 1;
+			}
+			memset( buffer_archivo, '\0', TAM );		
+			fgets(buffer_archivo, TAM, usuarios);		
+		}
+		if(intentos>1){
+			/// En caso de que no coincidan se avisa a destino que la clave o usuario no son correctos
+			n = write( newsockfd, "no", TAM ); // avisa al destino que no se encontro usuario/clave
+			if ( n < 0 ) { 
+				perror( "lectura de socket" );
+				exit( 1 );
+			}
+			
+		}
+		/// Se resta 1 a intentos y vuelve a empezar
+		intentos--;
+		fclose(usuarios);
+			
+	}
+	/// Si intentos llego a 0 quiere decir que se supero la cantidad de intentos, por lo tanto se le avisa al cliente
+	n = write( newsockfd, "superoIntentos", TAM);
+	if ( n < 0 ) {
+		perror( "lectura de socket" );
+		exit(1);
+	}
+	// fclose(usuarios);
+	return 0;
+}
